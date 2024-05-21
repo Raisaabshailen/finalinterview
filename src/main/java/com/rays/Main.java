f735380727408a2762bc20f1cee3f924c50582ff
raisaabshailen <shailendra87.tkg@gmail.com>
2024-05-22 02:37:44 +0530

    first commit

---
+++ src/main/java/com/rays/Main.java
@@ -0,0 +1,18 @@
+package com.rays;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        ArrayList l= new ArrayList();
+
+        l.add("ram");
+        l.add("shyam");
+        l.add("mohan");
+
+        System.out.println(l);
+    }
+}
